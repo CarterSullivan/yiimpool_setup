32e205647034ceb342e6eb8251aad4bfcab58383
Afiniel <afinieldev@outlook.com>
2022-02-11 22:32:38 -0800

    tag

--- menu.sh
+++ menu.sh
@@ -5,17 +5,23 @@
 
 source /etc/functions.sh
 
-RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Afiniel Crypto pool installer." --menu "Choose one" -1 60 16 \
-
-1 "YiiMP Single Server Good choice for beginners." \
-2 "YiiMP Multi Server More advanced options for beginners." \
+RESULT=$(dialog --stdout --nocancel --default-item 1 --title "Afiniel Crypto Pool Installer." --menu "Choose one" -1 60 16 \
+' ' "- YiiMP Server Install -" \
+1 "YiiMP Single Server" \
+2 "YiiMP Multi Server" \
 ' ' "- YiiMP Upgrade -" \
 3 "YiiMP Stratum Upgrade" \
 ' '  "- NOMP Server Install -" \
 4 "NOMP Server" \
+' ' "- MPOS Server Install -" \
+5 "MPOS Server - Coming Soon" \
+' '  "- CryptoNote Server Install -" \
+6 "CryptoNote-Nodejs Server - Coming Soon" \
+' ' "- Faucet Server Install -" \
+7 "Faucet Script - Coming Soon" \
 ' ' "- Daemon Wallet Builder -" \
-5 "Daemonbuilder" \
-6 Exit)
+8 "Daemonbuilder" \
+9 Exit)
 if [ $RESULT = ]
 then
 bash $(basename $0) && exit;
@@ -25,39 +31,60 @@ fi
 if [ $RESULT = 1 ]
 then
 clear;
-cd $HOME/yiimpool/install
+cd $HOME/multipool/install
 source bootstrap_single.sh;
 fi
 
 if [ $RESULT = 2 ]
 then
 clear;
-cd $HOME/yiimpool/install
+cd $HOME/multipool/install
 source bootstrap_multi.sh;
 fi
 
 if [ $RESULT = 3 ]
 then
 clear;
-cd $HOME/yiimpool/install
+cd $HOME/multipool/install
 source bootstrap_upgrade.sh;
 fi
 
-if [ $RESULT = 4 ]; 
+if [ $RESULT = 4 ]
 then
 clear;
-cd $HOME/yiimpool/install
-source bootstrap_nomp.sh
+cd $HOME/multipool/install
+source bootstrap_nomp.sh;
 fi
 
 if [ $RESULT = 5 ]
 then
 clear;
-cd $HOME/yiimpool/install
-source bootstrap_coin.sh
+cd $HOME/multipool/install
+exit 0;
 fi
 
 if [ $RESULT = 6 ]
+then
+clear;
+cd $HOME/multipool/install
+exit 0;
+fi
+
+if [ $RESULT = 7 ]
+then
+clear;
+cd $HOME/multipool/install
+exit 0;
+fi
+
+if [ $RESULT = 8 ]
+then
+clear;
+cd $HOME/multipool/install
+source bootstrap_coin.sh;
+fi
+
+if [ $RESULT = 9 ]
 then
 clear;
 exit;
